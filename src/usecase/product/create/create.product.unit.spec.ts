8026c7bcba96cc09e3fe731a9c4657d3a0805f23
andreluizsilva <andre.luiz.s@outlook.com>
2023-02-25 10:26:41 -0300

    update commit

---
+++ src/usecase/product/create/create.product.unit.spec.ts
@@ -0,0 +1,31 @@
+import { string } from "yup"
+import CreateProductUseCase from "./create.product.usecase"
+
+const input = {
+    name: "Product 1",
+    price: 30
+}
+
+const MockRepository = () =>{
+    return {
+        find: jest.fn(),
+        findAll: jest.fn(),
+        create: jest.fn(),
+        update: jest.fn(),
+    }
+}
+
+describe("Unit test create product use case", () => {
+    it("Should create a product", async() =>{
+        const productRepository = MockRepository();
+        const productCreateUseCase = new CreateProductUseCase(productRepository);
+
+        const output = await productCreateUseCase.execute(input)
+
+        expect(output).toEqual({
+            id: expect.any(String),
+            name: input.name,
+            price: input.price
+        })
+    })
+}) 
---
+++ src/usecase/product/find/find.product.dto.ts
@@ -0,0 +1,9 @@
+export interface InputFindProductDto {
+    id: string;
+}
+
+export interface outputFindProductDto {
+    id: string;
+    name: string;
+    price: number;
+}
